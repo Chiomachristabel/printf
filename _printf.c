7da83734c47f48bc5c879061677d05460dc60826
“Cristabelchioma” <okpetahchioma@gmail.com>
2023-05-30 13:10:36 -0700

    PRINTF PROJECTS

--- _printf.c
+++ _printf.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
-*_printf - produces output in accordance to a format
-*@format: the format string
-*
-*Return: Printed chars.
-*/
-
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -20,6 +18,7 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(list, format);
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -27,8 +26,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-
-			/*write(1, &format[i], 1);*/
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -40,13 +38,29 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
